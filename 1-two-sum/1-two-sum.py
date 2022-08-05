73bedf07fa3e6a634e37c8c9f7da55c9d3765018
hyuneie <109947783+hyuneie@users.noreply.github.com>
2022-08-05 11:27:56 +0900

    Time: 63 ms (95.14%), Space: 14.9 MB (95.53%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,8 +5,42 @@ class Solution:
          #        if nums[i] + nums[j] == target:
          #            return [i, j]
             
-        pairs = [target - nums for nums in nums]
+#         pairs = [target - nums for nums in nums]
         
-        for i, pair in enumerate(pairs):
-            if pair in nums and i != nums.index(pair):
-                return [i, nums.index(pair)]
+#         for i, pair in enumerate(pairs):
+#             if pair in nums and i != nums.index(pair):
+#                 return [i, nums.index(pair)]
+            
+        # HashTable = dict()
+        # for i, value in enumerate(nums):
+        #     print(i, value)
+        #     HashTable[value] = i
+        # Pairs = [target - num for num in nums ]
+        # for i, Pair in enumerate(Pairs):
+        #     if Pair in HashTable and i != HashTable[Pair]:
+        #         return [i, HashTable[Pair]]
+        # #print(Pairs)
+        
+        nums_sorted = sorted(nums) #2,7,11,15,3,-6 / 10으로 예시변경
+        print(nums_sorted)
+        
+        i, j = 0, len(nums)-1
+        
+        while i < j:
+            left = nums_sorted[i] 
+            right = nums_sorted[j]
+            difference = left + right - target
+            
+            if difference > 0:
+                j -= 1
+            elif difference < 0:
+                i += 1
+            else: # difference == 0
+                break
+                
+        if left != right:
+            return nums.index(left), nums.index(right)
+        else:
+            return [i for i, num in enumerate(nums) if num == left]
+        
+        
